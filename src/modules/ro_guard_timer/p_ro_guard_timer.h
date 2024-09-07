7ff5beafdcc317ac0adecce1e3856e744795bd9e
HardenedVault <root@hardenedvault.net>
2024-09-07 22:58:50 +0300

    update info

--- src/modules/ro_guard_timer/p_ro_guard_timer.h
+++ src/modules/ro_guard_timer/p_ro_guard_timer.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2021, HardenedVault Limited (https://hardenedvault.net)
+// Copyright (C) 2021-2024, HardenedVault (https://hardenedvault.net)
 
 void ro_guard_timer_init(void);
 extern struct timer_list ro_guard_timer;
